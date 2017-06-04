37e095f6f328c7d20dc4aed1c73658b532502ad2
Tomasz <Tomasz@192.168.0.227>
2017-06-04 15:19:55 +0200

    Lesson #5 - The Knight II

--- Chess/src/com/chess/engine/pieces/Knight.java
+++ Chess/src/com/chess/engine/pieces/Knight.java
@@ -1,10 +1,12 @@
 package com.chess.engine.pieces;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import com.chess.engine.Alliance;
 import com.chess.engine.board.Board;
+import com.chess.engine.board.BoardUtils;
 import com.chess.engine.board.Move;
 import com.chess.engine.board.Tile;
 import com.google.common.collect.ImmutableList;
@@ -20,17 +22,24 @@ public class Knight
     }
 
     @Override
-    public List<Move> calculateLegalMoves( Board board )
+    public Collection<Move> calculateLegalMoves( Board board )
     {
-        int candidateDestinationCoordinate;
         final List<Move> legalMoves = new ArrayList<Move>();
 
         for ( final int currentCandidate : CANDIDATE_MOVE_COORDINATES )
         {
-            candidateDestinationCoordinate = this.piecePosition + currentCandidate;
+            final int candidateDestinationCoordinate = this.piecePosition + currentCandidate;
 
-            if ( true )
+            if ( BoardUtils.isValidTileCoordinate( candidateDestinationCoordinate ) )
             {
+                if ( isFirstColumnExclusion( this.piecePosition, currentCandidate )
+                    || isSecondColumnExclusion( this.piecePosition, currentCandidate )
+                    || isSeventhColumnExclusion( this.piecePosition, currentCandidate )
+                    || isEighthColumnExclusion( this.piecePosition, currentCandidate ) )
+                {
+                    continue;
+                }
+
                 final Tile candidateDestinationTile = board.getTile( candidateDestinationCoordinate );
 
                 if ( !candidateDestinationTile.isTileOccupied() )
@@ -50,5 +59,27 @@ public class Knight
             }
         }
         return ImmutableList.copyOf( legalMoves );
+    }
+
+    private static boolean isFirstColumnExclusion( final int currentPosition, final int candidateOffset )
+    {
+        return BoardUtils.FIRST_COLUMN[currentPosition]
+            && ( candidateOffset == -17 || candidateOffset == -10 || candidateOffset == 6 || candidateOffset == 15 );
+    }
+
+    private static boolean isSecondColumnExclusion( final int currentPosition, final int candidatteOffset )
+    {
+        return BoardUtils.SECOND_COLUMN[currentPosition] && ( candidatteOffset == -10 || candidatteOffset == 6 );
+    }
+
+    private static boolean isSeventhColumnExclusion( final int currentPosition, final int candidatteOffset )
+    {
+        return BoardUtils.SEVENTH_COLUMN[currentPosition] && ( candidatteOffset == -6 || candidatteOffset == 10 );
+    }
+
+    private static boolean isEighthColumnExclusion( final int currentPosition, final int candidatteOffset )
+    {
+        return BoardUtils.EIGHTH_COLUMN[currentPosition] && ( candidatteOffset == -15 || candidatteOffset == -6
+            || candidatteOffset == 10 || candidatteOffset == 17 );
     }
 }
--- Chess/src/com/chess/engine/pieces/Piece.java
+++ Chess/src/com/chess/engine/pieces/Piece.java
@@ -1,6 +1,6 @@
 package com.chess.engine.pieces;
 
-import java.util.List;
+import java.util.Collection;
 
 import com.chess.engine.Alliance;
 import com.chess.engine.board.Board;
@@ -28,5 +28,5 @@ public abstract class Piece
         this.pieceAlliance = pieceAlliance;
     }
 
-    public abstract List<Move> calculateLegalMoves( final Board board );
+    public abstract Collection<Move> calculateLegalMoves( final Board board );
 }
