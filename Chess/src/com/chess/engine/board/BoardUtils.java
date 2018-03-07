578b43a0aca0003aad2a35b3e4286e1a92dea28b
Tomasz <tomrowicki@gmail.com>
2018-03-07 18:54:49 +0100

    Lesson #37 - GUI PART IX

--- Chess/src/com/chess/engine/board/BoardUtils.java
+++ Chess/src/com/chess/engine/board/BoardUtils.java
@@ -1,6 +1,11 @@
 package com.chess.engine.board;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 
 public class BoardUtils {
 
@@ -32,9 +37,11 @@ public class BoardUtils {
 
 	public static final int NUM_TILES_PER_ROW = 8;
 
-	public static final String ALGEBRAIC_NOTATION[] = initializeAlgebraicNotation();
+	public static final List<String> ALGEBRAIC_NOTATION = initializeAlgebraicNotation();
 
 	public static final Map<String, Integer> POSITION_TO_COORDINATE = initializePositionToCoordinateMap();
+
+	public static final int START_TILE_INDEX = 0;
 
 	private static boolean[] initColumn(int columnNumber) {
 		final boolean[] column = new boolean[NUM_TILES];
@@ -62,7 +69,30 @@ public class BoardUtils {
 		return POSITION_TO_COORDINATE.get(position);
 	}
 
-	public static String getPositionAtCoordinate(final int destinationCoordinate) {
-		return ALGEBRAIC_NOTATION[destinationCoordinate];
+	public static String getPositionAtCoordinate(final int coordinate) {
+		return ALGEBRAIC_NOTATION.get(coordinate);
 	}
+
+	private static Map<String, Integer> initializePositionToCoordinateMap() {
+		final Map<String, Integer> positionToCoordinate = new HashMap<>();
+		for (int i = START_TILE_INDEX; i < NUM_TILES; i++) {
+			positionToCoordinate.put(ALGEBRAIC_NOTATION.get(i), i);
+		}
+		return ImmutableMap.copyOf(positionToCoordinate);
+	}
+
+	// @formatter:off
+    private static List<String> initializeAlgebraicNotation() {
+        return ImmutableList.copyOf(new String[]{
+                "a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8",
+                "a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7",
+                "a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6",
+                "a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5",
+                "a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4",
+                "a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3",
+                "a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2",
+                "a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1"
+        });
+    }
+    // @formatter:on
 }
