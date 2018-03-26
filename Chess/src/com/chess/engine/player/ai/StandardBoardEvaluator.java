27ccdd3724471f9e3eebd2773be02be283698075
Tomasz <tomrowicki@gmail.com>
2018-03-26 18:56:16 +0200

    Lesson #49 - MiniMax Part V

--- Chess/src/com/chess/engine/player/ai/StandardBoardEvaluator.java
+++ Chess/src/com/chess/engine/player/ai/StandardBoardEvaluator.java
@@ -6,13 +6,41 @@ import com.chess.engine.player.Player;
 
 public final class StandardBoardEvaluator implements BoardEvaluator {
 
+	private static final int CHECK_BONUS = 50;
+
+	private static final int CHECK_MATE_BONUS = 10000;
+
+	private static final int DEPTH_BONUS = 100;
+
+	private static final int CASTLE_BONUS = 60;
+
 	@Override
 	public int evaluate(final Board board, final int depth) {
 		return scorePlayer(board, board.whitePlayer(), depth) - scorePlayer(board, board.blackPlayer(), depth);
 	}
 
-	private int scorePlayer(final Board board, final Player blackPlayer, final int depth) {
-		return pieceValue(player);
+	private int scorePlayer(final Board board, final Player player, final int depth) {
+		return pieceValue(player) + mobility(player) + check(player) + checkmate(player, depth) + castled(player);
+	}
+
+	private static int castled(final Player player) {
+		return player.isCastled() ? CASTLE_BONUS : 0;
+	}
+
+	private static int checkmate(final Player player, final int depth) {
+		return player.getOpponent().isInCheckMate() ? CHECK_MATE_BONUS * depthBonus(depth) : 0;
+	}
+
+	private static int depthBonus(final int depth) {
+		return depth == 0 ? 1 : DEPTH_BONUS * depth;
+	}
+
+	private static int check(final Player player) {
+		return player.getOpponent().isInCheck() ? CHECK_BONUS : 0;
+	}
+
+	private static int mobility(final Player player) {
+		return player.getLegalMoves().size();
 	}
 
 	private static int pieceValue(final Player player) {
